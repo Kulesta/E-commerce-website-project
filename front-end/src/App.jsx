5b1da5d4cdbbdc95fadfbee2a6889f9924fdd2d8
MWD-Tech <mwdtech369@gmail.com>
2025-11-28 03:54:25 -0800

    Add front-end folder and update package.json

---
+++ front-end/src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import PlaceOrder from './pages/PlaceOrder'
+import Orders from './pages/Orders'
+import Login from './pages/Login'
+import Product from './pages/Product'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/placeOrder' element={<PlaceOrder/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/product/:productId' element={<Product/>} />
+      </Routes>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
